e56870e7adcc0508cc862297224f934bfa9a6f77
root <root@DESKTOP-V72IK3U.localdomain>
2020-03-10 11:28:53 +0100

    initial commit

---
+++ CallConverter/CallConverter.cs
@@ -0,0 +1,176 @@
+﻿using Grpc.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using WebSocketSharp.Net.WebSockets;
+
+namespace GrpctoWebsocketProxy.CallConverterNs
+{
+    class CallConverter
+    {
+        private object client;
+        WebSocketContext socketContext;
+        private string requestId;
+        public CallConverter(object client)
+        {
+            this.client = client;
+
+        }
+
+        public CallConverter(object client, WebSocketContext cnx, string requestId = "")
+        {
+            this.client = client;
+            this.socketContext = cnx;
+            this.requestId = requestId;
+        }
+
+
+        public async Task<object> CallMethod(string method, string jsonParameters)
+        {
+            MethodInfo CurrentMethod = CreateMethod(method);
+
+            if (string.IsNullOrEmpty(jsonParameters))
+            {
+                jsonParameters = new JObject().ToString();
+            }
+
+            if (CurrentMethod == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "Method does not exist. Remember its case sensitive."));
+            }
+
+
+            Type CurrentParameterType = CreateParameterType(CurrentMethod);
+
+            object Deserialized = DeserializeFromJson(jsonParameters, CurrentParameterType);
+
+            try
+            {
+                dynamic methodCall;
+
+
+                if (CurrentMethod.Name.ToLower().Contains("async"))
+                {
+                    throw new RpcException(new Status(StatusCode.Unavailable, "Async grpc methods currently unavailable."));
+
+
+                }
+                else
+                {
+                    methodCall = InvokeMethod(CurrentMethod, Deserialized);
+                }
+
+                var responseStreamProperty = methodCall.GetType().GetProperty("ResponseStream");
+                if (responseStreamProperty != null)
+                {
+                    List<object> listOfItems = new List<object>();
+                    IAsyncStreamReader<object> asyncStreamReader = (IAsyncStreamReader<object>)responseStreamProperty.GetValue(methodCall);
+                    try
+                    {
+                        while (await asyncStreamReader.MoveNext())
+                        {
+                            if (socketContext != null)
+                            {
+                                JObject messageClear = new JObject();
+                                messageClear.Add(new JProperty("requestId", requestId));
+                                messageClear.Add(new JProperty("currentMessage", asyncStreamReader.Current.ToString()));
+                                socketContext.WebSocket.Send(messageClear.ToString());
+                            }
+
+                            listOfItems.Add(asyncStreamReader.Current);
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        throw new RpcException(new Status(StatusCode.NotFound, "Stream was empty"));
+
+                    }
+
+                    return listOfItems;
+                }
+                else
+                {
+                    if (socketContext != null)
+                    {
+                        socketContext.WebSocket.Send(methodCall.ToString());
+
+                    }
+                    return methodCall;
+                }
+            }
+            catch (Exception e)
+            {
+                new RpcException(new Status(StatusCode.Internal, e.Message));
+            }
+            throw new RpcException(new Status(StatusCode.Internal, "unknown"));
+
+        }
+
+        public JObject ImageBytesToBase64(string fieldName, JObject dataObject)
+        {
+
+
+            if (dataObject.ContainsKey(fieldName))
+            {
+                byte[] intArray = dataObject[fieldName].ToObject<byte[]>();
+                string bytesString = Convert.ToBase64String(intArray);
+                var byteString = Google.Protobuf.ByteString.CopyFrom(intArray);
+                dataObject[fieldName] = byteString.ToBase64();
+            }
+
+            return dataObject;
+        }
+        private MethodInfo CreateMethod(string method)
+        {
+            MethodInfo[] methodInfos = client.GetType()
+                           .GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+
+            var interestedMethod = methodInfos.Where(x => x.GetParameters().Length <= 2)
+                .Where(x => x.Name.ToLower() == method.ToLower())
+                .FirstOrDefault();
+
+
+            return interestedMethod;
+
+        }
+
+        private Type CreateParameterType(MethodInfo interestedMethod)
+        {
+
+            ParameterInfo[] methodParameterInfos = interestedMethod
+                .GetParameters();
+
+            ParameterInfo methodParameterInfo = methodParameterInfos[0];
+
+            Type methodParameterType = methodParameterInfo.ParameterType;
+
+
+
+            return methodParameterType;
+        }
+
+        private object DeserializeFromJson(string json, Type methodParameterType)
+        {
+            return JsonConvert.DeserializeObject(json, methodParameterType);
+        }
+
+
+        private object InvokeMethod(MethodInfo method, object deserializedJson)
+        {
+
+
+            var result = method.Invoke(client, new object[] { deserializedJson, new Grpc.Core.CallOptions() });
+            return result;
+
+        }
+
+
+
+
+    }
+}
---
+++ GrpcToWebsocket/GRPCWebsocket.cs
@@ -0,0 +1,145 @@
+﻿using Grpc.Core;
+using Newtonsoft.Json.Linq;
+using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using GrpctoWebsocketProxy.CallConverterNs;
+namespace GrpctoWebsocketProxy.GrpcToWebsocket
+{
+    public class GrpcWebSocket : WebSocketBehavior
+    {
+
+        public GrpcWebSocket()
+        {
+
+        }
+
+        public GrpcWebSocket(ClientBase cli)
+        {
+
+            this.cli = cli;
+        }
+
+        ClientBase cli;
+
+        protected override void OnOpen()
+        {
+
+            base.OnOpen();
+        }
+
+        protected async override void OnMessage(MessageEventArgs e)
+        {
+
+            var jObj = JObject.Parse(e.Data);
+            string method = "";
+            string body = "";
+            string requestId = "";
+
+            try
+            {
+                requestId = jObj["requestId"].ToString();
+            }
+            catch (NullReferenceException ex)
+            {
+                requestId = "";
+            }
+
+            try
+            {
+                method = jObj["method"].ToString();
+            }
+            catch (NullReferenceException ex)
+            {
+                JObject methodNullError = new JObject();
+                methodNullError.Add(
+                    new JProperty(
+                        "error", "\"method\" property can't be null")
+                    );
+                methodNullError.Add(
+                   new JProperty(
+                       "advice", "\"method\" is your Grpc Method name, \"requestId\" is id of request (use if want to make a lot of calls) and \"body\" is payload for your method.")
+                   );
+                methodNullError.Add(
+                    new JProperty("requestId", requestId)
+                    );
+                Send(methodNullError.ToString());
+                return;
+
+            }
+
+            try
+            {
+                body = jObj["body"].ToString();
+            }
+            catch (NullReferenceException ex)
+            {
+                body = "{}";
+            }
+
+
+
+
+            try
+            {
+
+                CallConverter converter = new CallConverter(cli, this.Context, requestId);
+                await converter.CallMethod(method, body);
+            }
+            catch (RpcException er)
+            {
+                JObject rpcExceptionData = new JObject();
+                rpcExceptionData.Add(
+                    new JProperty("error", "RpcException")
+                    );
+                rpcExceptionData.Add(
+                    new JProperty("statusCode", er.StatusCode)
+                    );
+                rpcExceptionData.Add(
+                    new JProperty("details", er.Status.Detail)
+                    );
+                rpcExceptionData.Add(
+                    new JProperty("requestId", requestId)
+                    );
+
+
+
+
+
+                Send(rpcExceptionData.ToString());
+            }
+            catch (Exception er)
+            {
+
+                JObject unknownException = new JObject();
+                unknownException.Add(new JProperty("error", "AnotherException"));
+                unknownException.Add(
+                    new JProperty("statusCode", 500)
+                    );
+                unknownException.Add(new JProperty("details", er.InnerException.Message));
+                unknownException.Add(
+                    new JProperty("requestId", requestId)
+                    );
+
+
+                Send(er.Message);
+            }
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+            JObject socketException = new JObject();
+
+            socketException.Add(new JProperty("error", "WebSocketException"));
+            socketException.Add(
+                    new JProperty("statusCode", 501)
+                    );
+            socketException.Add(new JProperty("details", e.Message));
+
+
+            Send(e.Message);
+        }
+
+
+    }
+}
